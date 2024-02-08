97317041e5074fd126ca11799632fb10c2021903
alharibat@yahoo.com <alharibat@yahoo.com>
2024-02-08 17:12:12 +0300

    modifying SampleTest2 class

--- src/test/java/ju/edu/jo/test/SampleTest2.java
+++ src/test/java/ju/edu/jo/test/SampleTest2.java
@@ -1,1 +1,18 @@
-SDSDSD
+package ju.edu.jo.test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.runner.RunWith;
+
+@RunWith(JUnitPlatform.class)
+public class SampleTest2 {
+
+  @Test
+  public void testEquality() {
+	System.out.println("Test 3");
+    int no1 = 1;
+    int no2 = 3;
+    assertEquals(no1 , no2);
+  }
+
+}
